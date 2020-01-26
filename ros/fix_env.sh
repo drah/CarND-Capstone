d5b8792a76c67dbc5eac166f8ed514a3c279a1bf
drah <drah2easy@gmail.com>
2020-01-26 09:37:47 +0000

    Add: initialize.sh and fix_env.sh

---
+++ ros/fix_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ros-kinetic-dbw-mkz-msgs
+sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install ros-kinetic-dbw-mkz-msgs
---
+++ ros/initialize.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config user.name drah
+git config user.email drah2easy@gmail.com
+
+sh fix_env.sh
+
+
